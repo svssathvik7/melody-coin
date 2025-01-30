77ce8a2ba2c7fd2763c9ea4fb5d9a3b268dd7512
SATHVIK SVS <svssathvik77@gmail.com>
2025-01-30 12:30:25 +0530

    add viem client

--- src/config/viemConfig.ts
+++ src/config/viemConfig.ts
@@ -1,16 +1,18 @@
 import { CONTRACT_ADDRESS, MELODY_COIN_ABI } from "@/constants/contractDetails";
-import { createPublicClient, getContract, http } from "viem";
+import { createClient, getContract, http } from "viem";
 import { sepolia } from "viem/chains";
 
-const publicClient = createPublicClient({
+const userAddress = "0x3e6456985c2D1641ca11CDBEc5934B4DDb82f505";
+const client = createClient({
+  account: userAddress,
   chain: sepolia,
-  transport: http(),
+  transport: http(process.env.NEXT_PUBLIC_RPC_URL),
 });
 
 const contract = getContract({
   address: CONTRACT_ADDRESS,
   abi: MELODY_COIN_ABI,
-  client: publicClient,
+  client,
 });
 
 export default contract;
--- src/utils/contractFetcher.ts
+++ src/utils/contractFetcher.ts
@@ -2,6 +2,7 @@ import contract from "@/config/viemConfig";
 
 export const getBalance = async (address: string) => {
   try {
+    console.log("address : ", address);
     const balance = await contract.read.balanceOf([address]);
     // Ensure both values are of type BigInt (uppercase)
     const balanceInTokens: bigint = balance as bigint;
