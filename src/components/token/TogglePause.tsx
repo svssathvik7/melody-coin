5bb193c5df2314dcdfd09a300d489b210920464b
SATHVIK SVS <svssathvik77@gmail.com>
2025-02-03 13:33:04 +0530

    update toggle pause owner validation

--- src/components/token/TogglePause.tsx
+++ src/components/token/TogglePause.tsx
@@ -9,7 +9,7 @@ import {
   CardFooter,
   CardDescription,
 } from "@/components/ui/card";
-import { getPauseState } from "@/utils/contractFetcher";
+import { getContractOwner, getPauseState } from "@/utils/contractFetcher";
 import { Button } from "@/components/ui/button";
 import toaster from "@/utils/toaster";
 import {
@@ -30,11 +30,20 @@ const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
 export default function TogglePause() {
   const { address } = useAccount();
   const [isPaused, setIsPaused] = useState(true);
+  const [isOwner, setIsOwner] = useState(false);
   const addRecentTransaction = useAddRecentTransaction();
   const { data: hash, writeContract } = useWriteContract();
   const { isLoading: isConfirming } = useWaitForTransactionReceipt({
     hash,
   });
+
+  useEffect(() => {
+    const fetchContractOwner = async () => {
+      const owner = await getContractOwner();
+      setIsOwner(owner == address);
+    };
+    fetchContractOwner();
+  }, [address]);
 
   const fetchPauseState = async () => {
     const state = await getPauseState();
@@ -122,55 +131,59 @@ export default function TogglePause() {
         </div>
       </CardContent>
       <CardFooter>
-        <Button
-          onClick={togglePauseState}
-          disabled={isConfirming}
-          className={`w-full transition-all duration-300 transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-offset-2 ${
-            isPaused
-              ? "bg-white text-black border-2 border-black hover:bg-black hover:text-white focus:ring-black"
-              : "bg-black text-white hover:bg-gray-800 focus:ring-black"
-          } shadow-lg`}
-        >
-          {isConfirming ? (
-            <span className="flex items-center justify-center">
-              <svg
-                className="animate-spin -ml-1 mr-3 h-5 w-5 text-current"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <circle
-                  className="opacity-25"
-                  cx="12"
-                  cy="12"
-                  r="10"
-                  stroke="currentColor"
-                  strokeWidth="4"
-                ></circle>
-                <path
-                  className="opacity-75"
-                  fill="currentColor"
-                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                ></path>
-              </svg>
-              Processing...
-            </span>
-          ) : (
-            <>
-              {isPaused ? (
-                <>
-                  <PlayIcon className="mr-2 h-5 w-5 animate-pulse" />
-                  Resume Contract
-                </>
-              ) : (
-                <>
-                  <PauseIcon className="mr-2 h-5 w-5 animate-pulse" />
-                  Pause Contract
-                </>
-              )}
-            </>
-          )}
-        </Button>
+        {isOwner ? (
+          <Button
+            onClick={togglePauseState}
+            disabled={isConfirming}
+            className={`w-full transition-all duration-300 transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-offset-2 ${
+              isPaused
+                ? "bg-white text-black border-2 border-black hover:bg-black hover:text-white focus:ring-black"
+                : "bg-black text-white hover:bg-gray-800 focus:ring-black"
+            } shadow-lg`}
+          >
+            {isConfirming ? (
+              <span className="flex items-center justify-center">
+                <svg
+                  className="animate-spin -ml-1 mr-3 h-5 w-5 text-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <circle
+                    className="opacity-25"
+                    cx="12"
+                    cy="12"
+                    r="10"
+                    stroke="currentColor"
+                    strokeWidth="4"
+                  ></circle>
+                  <path
+                    className="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                  ></path>
+                </svg>
+                Processing...
+              </span>
+            ) : (
+              <>
+                {isPaused ? (
+                  <>
+                    <PlayIcon className="mr-2 h-5 w-5 animate-pulse" />
+                    Resume Contract
+                  </>
+                ) : (
+                  <>
+                    <PauseIcon className="mr-2 h-5 w-5 animate-pulse" />
+                    Pause Contract
+                  </>
+                )}
+              </>
+            )}
+          </Button>
+        ) : (
+          <p className="text-red-500 text-xs">Require owner access</p>
+        )}
       </CardFooter>
     </Card>
   );
