dc60bfea5882f5f9a0ec268ff5a629e7c3652fb7
SATHVIK SVS <svssathvik77@gmail.com>
2025-03-24 23:46:48 +0530

    update check balance div height

--- src/components/token/CheckBalance.tsx
+++ src/components/token/CheckBalance.tsx
@@ -57,7 +57,7 @@ export default function CheckBalance() {
   })
 
   return (
-    isConnected && <Card className="w-full max-w-md mx-auto h-[55dvh] my-8 text-black bg-white shadow-lg overflow-y-scroll">
+    isConnected && <Card className="w-full max-w-md mx-auto h-fit my-8 text-black bg-white shadow-lg overflow-y-scroll">
       <CardHeader className="text-center">
         <Image
           className="w-24 h-24 mx-auto mb-4 rounded-full shadow-md"
